890fb07a0126398a8c2b01901cfc5fc7e43a6925
Dima Chaika <dchaika1990@gmail.com>
2019-10-01 00:35:50 +0300

    start project

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,16 @@ import React from "react";
 
 export class App extends React.Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <h1>Top Commented</h1>
+        <div>
+          <img src="" alt=""/>
+          <p>Title</p>
+          <p>Number of comments: 123</p>
+          <a href="" target='_blank'></a>
+        </div>
+      </div>
+    )
   }
 }
