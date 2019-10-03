2f57cb901981812079cd8a73d4aaf3e08a0c62bd
dchaika <dchaika1990@gmail.com>
2019-10-03 16:27:38 +0300

    add item component and add it to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,55 @@
 import React from "react";
+import { Item } from "./Item";
 
 export class App extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      items: [],
+      isLoading: false,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    fetch('https://www.reddit.com/r/reactjs.json?limit=100')
+    .then((response) => response.json())
+    .then( ({data}) => {
+      this.setState({ 
+        items: data.children,
+        isLoading: false
+       });
+    } );
+  }
+
+  updateAutoRefresh = () => {
+
+  } 
+
   render() {
+
+    const {items, isLoading} = this.state;
+    const itemsSortByComments = items.sort( 
+      (a,b) => b.data.num_comments - a.data.num_comments
+    );
     return (
       <div>
         <h1>Top Commented</h1>
-        <div>
-          <img src="" alt=""/>
-          <p>Title</p>
-          <p>Number of comments: 123</p>
-          <a href="" target='_blank'></a>
-        </div>
+        <button 
+          type="button"
+          onClick={this.updateAutoRefresh}
+          style={{
+            marginBottom: '15px'
+          }}
+        >
+          Start AutoRefresh
+        </button>
+        { isLoading ? <p>...Loading</p> : (
+          itemsSortByComments.map(
+            item => <Item key={item.data.id} data={item.data} /> 
+          )
+        )}
       </div>
     )
   }
---
+++ src/components/Item.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export class Item extends Component {
+    render() {
+        const {data} = this.props;
+        return (
+            <div 
+                style={{
+                  border: '1px solid black', 
+                  marginTop: '30px', 
+                  padding: '25px'
+                }}
+              >
+                { data.thumbnail !=='self' && (
+                    <img src={data.thumbnail} alt=""/>
+                ) }
+                <p>{data.title}</p>
+                <p>Number of comments: {data.num_comments}</p>
+                <a href={'https://www.reddit.com'+data.permalink} 
+                  target='_blank' 
+                  rel="noopener noreferrer"
+                >
+                  link
+                </a>
+            </div>
+        );
+    }
+}
